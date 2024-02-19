a70c1632679ef658d9914f0defb3e3e0630167a3
eryajf <eryajf@163.com>
2024-02-19 18:26:09 +0800

    fix: 修复多级目录时显示异常的问题

---
+++ src/views/system/log/index.vue
@@ -0,0 +1,4 @@
+<!-- 如果路由有多个层级，那么此处需要如下代码，详见：https://panjiachen.github.io/vue-element-admin-site/zh/guide/essentials/router-and-nav.html#%E5%A4%9A%E7%BA%A7%E7%9B%AE%E5%BD%95-%E5%B5%8C%E5%A5%97%E8%B7%AF%E7%94%B1 -->
+<template>
+  <router-view />
+</template>
